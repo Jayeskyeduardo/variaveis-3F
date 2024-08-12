9f9455f8ec7226ec9a8d49a76ea0f86bc0d416f9
JayeskyEduardo <164092285+Jayeskyeduardo@users.noreply.github.com>
2024-08-12 23:10:53 +0000

    12/08/24-2

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const idade = 18;
+let idade = 18;
 const anoNascimento = 2006;
 let anoAtual = 2024;
+const nomeCompleto = "Jayeky Perdigón";
 
+console.log("Meu nome é " + nomeCompleto + " nacido no ano de " + anoNascimento + " e no ano atual " + anoAtual +" completo " + idade + " anos");
 anoAtual = anoAtual +1;
+idade = idade +1;
 
 console.log(idade);
 console.log(anoAtual);
