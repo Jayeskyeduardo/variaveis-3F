a9087b5b592db5771a637d4bce9517548a52af56
JayeskyEduardo <164092285+Jayeskyeduardo@users.noreply.github.com>
2024-08-26 23:05:24 +0000

    26/08/24

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 let idade = 18;
 const anoNascimento = 2006;
 let anoAtual = 2024;
-const nomeCompleto = "Jayeky Perdigón";
+const nomeCompleto = "Jayesky Perdigón";
 
 console.log("Meu nome é " + nomeCompleto + " nacido no ano de " + anoNascimento + " e no ano atual " + anoAtual +" completo " + idade + " anos");
+
 anoAtual = anoAtual +1;
 idade = idade +1;
 
 console.log(idade);
 console.log(anoAtual);
+
+
+//Agência de viagem Tokyo Paris Cuba Madrid China 
+const listaDeDestinos = new Array(
+  "Tokyo",
+  "Paris",
+  "Cuba",
+  "Madrid",
+  "China"
+);
+
+console.log(listaDeDestinos);
+console.log(listaDeDestinos[3]);
+listaDeDestinos.push("Coronel Vivida");
+listaDeDestinos.push("Curitiba");
+listaDeDestinos.push("Rio Branco");
+listaDeDestinos.push("Berín");
+console.log(listaDeDestinos);
